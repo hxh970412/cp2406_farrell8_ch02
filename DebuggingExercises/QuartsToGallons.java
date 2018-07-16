d933bfb90d8636f127bb9d7c85c634c56c104bd9
jc445081 <xinhao.huang@my.jcu.edu.au>
2018-07-16 14:44:41 +0800

    prac 2

---
+++ DebuggingExercises/QuartsToGallons.java
@@ -0,0 +1,18 @@
+/**
+ * Created by jc445081 on 16/07/18.
+ */
+public class QuartsToGallons
+{
+    public static void main(String[] args)
+    {
+        int Quarts_in_Gallons = 4;
+        int QuartsNeeded = 18;
+        int GallonsNeeded;
+        int OneJob;
+        GallonsNeeded = QuartsNeeded / Quarts_in_Gallons;
+        
+        System.out.println("Value is " + Quarts_in_Gallons);
+
+
+    }
+}
